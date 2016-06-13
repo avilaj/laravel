d27c886380be82f0f85941a74d0858e447d7909f
Jorge Avila <jorge.avila@globant.com>
2016-06-12 23:42:37 -0300

    Home - principio de maquetado

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,43 @@
+<section id="footer">
+	{LOGO_MARK}
+
+	<ul>
+		<li><strong>Mapa del sitio</strong></li>
+		<li><a href="/novedades">Novedades</a></li>
+		<li><a href="/nosotros">Nosotros</a></li>
+		<li><a href="/catalogo">Catalogo</a></li>
+		<li><a href="/radio">Radio</a></li>
+		<li><a href="/locales">Locales</a></li>
+		<li><a href="/contacto">Contacto</a></li>
+	</ul>
+	<ul>
+		<li><strong>Mi cuenta</strong></li>
+		<li><a href="/registrarse">Registrarse</a></li>
+		<li><a href="/ingresar">Ingresar</a></li>
+		<li><a href="/mi-carro">Mi Carro</a></li>
+		<li><a href="/check-out">Check Out</a></li>
+		<li><a href="/locales">Locales</a></li>
+		<li><a href="/contacto">Contacto</a></li>
+	</ul>
+	<ul>
+		<li><strong>Servicio al cliente</strong></li>
+		<li><a href="/privacidad">Privacidad</a></li>
+		<li><a href="/terminos-condiciones">Términos y condiciones</a></li>
+		<li><a href="/mi-carro">Mi Carro</a></li>
+		<li><a href="/check-out">Check Out</a></li>
+	</ul>
+
+	<ul>
+		<li><strong>Seguinos en</strong></li>
+		<li><a href="#">Facebook</a></li>
+		<li><a href="#">Twitter</a></li>
+		<li><a href="#">Instagram</a></li>
+		<li><a href="#">Youtube</a></li>
+	</ul>
+
+	<ul>
+		<li><strong>Contactanos</strong></li>
+		<li><a href="#"> <i class="fa fa-phone"></i> 011 4776-8082</a></li>
+		<li><a href="#"><i class="fa fa-mail"></i> info@markshop.com.ar</a></li>
+	</ul>
+</section>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
-
-                <div class="panel-body">
-                    Your Application's Landing Page.
-                </div>
-            </div>
-        </div>
-    </div>
+<div class="slider">
+    <h3 class="slider__title">Productos destacados</h3>
+    <ul class="slider__list">
+        <li class="productBox">
+            <img class="productBox__image" src="http://placehold.it/240x300" alt="titulo de producto">
+            <strong class="productBox__title">Titulo del producto</strong>
+            <span class="productBox__price">$320.</span>
+        </li>
+        <li class="productBox-container">
+            <img class="productBox__image" src="http://placehold.it/240x300" alt="titulo de producto">
+            <strong class="productBox__title">Titulo del producto</strong>
+            <span class="productBox__price">$320.</span>
+        </li>
+        <li class="productBox-container">
+            <img class="productBox__image" src="http://placehold.it/240x300" alt="titulo de producto">
+            <strong class="productBox__title">Titulo del producto</strong>
+            <span class="productBox__price">$320.</span>
+        </li>
+        <li class="productBox-container">
+            <img class="productBox__image" src="http://placehold.it/240x300" alt="titulo de producto">
+            <strong class="productBox__title">Titulo del producto</strong>
+            <span class="productBox__price">$320.</span>
+        </li>
+        <li class="productBox-container">
+            <img class="productBox__image" src="http://placehold.it/240x300" alt="titulo de producto">
+            <strong class="productBox__title">Titulo del producto</strong>
+            <span class="productBox__price">$320.</span>
+        </li>
+        <li class="productBox-container">
+            <img class="productBox__image" src="http://placehold.it/240x300" alt="titulo de producto">
+            <strong class="productBox__title">Titulo del producto</strong>
+            <span class="productBox__price">$320.</span>
+        </li>
+        <li class="productBox-container">
+            <img class="productBox__image" src="http://placehold.it/240x300" alt="titulo de producto">
+            <strong class="productBox__title">Titulo del producto</strong>
+            <span class="productBox__price">$320.</span>
+        </li>
+        <li class="productBox-container">
+            <img class="productBox__image" src="http://placehold.it/240x300" alt="titulo de producto">
+            <strong class="productBox__title">Titulo del producto</strong>
+            <span class="productBox__price">$320.</span>
+        </li>
+        <li class="productBox-container">
+            <img class="productBox__image" src="http://placehold.it/240x300" alt="titulo de producto">
+            <strong class="productBox__title">Titulo del producto</strong>
+            <span class="productBox__price">$320.</span>
+        </li>
+    </ul>
 </div>
 @endsection
