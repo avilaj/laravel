41018925965928fc8b2b2b3c6ac81162ab6860da
Jorge Avila <jorge.avila@globant.com>
2016-06-13 02:36:46 -0300

    Footer layout

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,43 +1,51 @@
 <section id="footer">
-    {LOGO_MARK}
+    <div class="section">
+        {LOGO_MARK}
+    </div>
+    <div class="section">
+        <ul>
+            <li><strong>Mapa del sitio</strong></li>
+            <li><a href="/novedades">Novedades</a></li>
+            <li><a href="/nosotros">Nosotros</a></li>
+            <li><a href="/catalogo">Catalogo</a></li>
+            <li><a href="/radio">Radio</a></li>
+            <li><a href="/locales">Locales</a></li>
+            <li><a href="/contacto">Contacto</a></li>
+        </ul>
+    </div>
+    <div class="section">
+        <ul>
+            <li><strong>Mi cuenta</strong></li>
+            <li><a href="/registrarse">Registrarse</a></li>
+            <li><a href="/ingresar">Ingresar</a></li>
+            <li><a href="/mi-carro">Mi Carro</a></li>
+            <li><a href="/check-out">Check Out</a></li>
+            <li><a href="/locales">Locales</a></li>
+            <li><a href="/contacto">Contacto</a></li>
+        </ul>
+    </div>
+    <div class="section">
+        <ul>
+            <li><strong>Servicio al cliente</strong></li>
+            <li><a href="/privacidad">Privacidad</a></li>
+            <li><a href="/terminos-condiciones">Términos y condiciones</a></li>
+            <li><a href="/mi-carro">Mi Carro</a></li>
+            <li><a href="/check-out">Check Out</a></li>
+        </ul>
+    </div>
+    <div class="section">
+        <ul>
+            <li><strong>Seguinos en</strong></li>
+            <li><a href="#">Facebook</a></li>
+            <li><a href="#">Twitter</a></li>
+            <li><a href="#">Instagram</a></li>
+            <li><a href="#">Youtube</a></li>
+        </ul>
+        <ul>
+            <li><strong>Contactanos</strong></li>
+            <li><a href="#"> <i class="fa fa-phone"></i> 011 4776-8082</a></li>
+            <li><a href="#"><i class="fa fa-mail"></i> info@markshop.com.ar</a></li>
+        </ul>
+    </div>
 
-    <ul>
-        <li><strong>Mapa del sitio</strong></li>
-        <li><a href="/novedades">Novedades</a></li>
-        <li><a href="/nosotros">Nosotros</a></li>
-        <li><a href="/catalogo">Catalogo</a></li>
-        <li><a href="/radio">Radio</a></li>
-        <li><a href="/locales">Locales</a></li>
-        <li><a href="/contacto">Contacto</a></li>
-    </ul>
-    <ul>
-        <li><strong>Mi cuenta</strong></li>
-        <li><a href="/registrarse">Registrarse</a></li>
-        <li><a href="/ingresar">Ingresar</a></li>
-        <li><a href="/mi-carro">Mi Carro</a></li>
-        <li><a href="/check-out">Check Out</a></li>
-        <li><a href="/locales">Locales</a></li>
-        <li><a href="/contacto">Contacto</a></li>
-    </ul>
-    <ul>
-        <li><strong>Servicio al cliente</strong></li>
-        <li><a href="/privacidad">Privacidad</a></li>
-        <li><a href="/terminos-condiciones">Términos y condiciones</a></li>
-        <li><a href="/mi-carro">Mi Carro</a></li>
-        <li><a href="/check-out">Check Out</a></li>
-    </ul>
-
-    <ul>
-        <li><strong>Seguinos en</strong></li>
-        <li><a href="#">Facebook</a></li>
-        <li><a href="#">Twitter</a></li>
-        <li><a href="#">Instagram</a></li>
-        <li><a href="#">Youtube</a></li>
-    </ul>
-
-    <ul>
-        <li><strong>Contactanos</strong></li>
-        <li><a href="#"> <i class="fa fa-phone"></i> 011 4776-8082</a></li>
-        <li><a href="#"><i class="fa fa-mail"></i> info@markshop.com.ar</a></li>
-    </ul>
 </section>
