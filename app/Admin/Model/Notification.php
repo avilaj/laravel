e788479e2630d5910ddfd59daddd5a857e22e42c
Jorge Avila <jorge.e.avila@gmail.com>
2016-08-15 17:46:50 -0300

    follow ipn notifications

--- app/Admin/Model/Notification.php
+++ app/Admin/Model/Notification.php
@@ -6,6 +6,7 @@
  */
 use App\Model\Notification;
 use SleepingOwl\Admin\Model\ModelConfiguration;
+use \Illuminate\Database\Eloquent\Model;
 
 AdminSection::registerModel(Notification::class, function (ModelConfiguration $model) {
     $model->setTitle("Notificaciones");
@@ -19,8 +20,15 @@ AdminSection::registerModel(Notification::class, function (ModelConfiguration $m
             AdminColumn::text('id')->setLabel('#')->setWidth('50px'),
             AdminColumn::text('topic')->setLabel('Topic'),
             AdminColumn::text('identificator')->setLabel('Id'),
-            AdminColumn::custom(function ($instance) {
-              return 'hello';
+            AdminColumn::custom()->setCallback(function (Model $model) {
+              $params = ['id'=> $model->identificator];
+              if ($model->topic == "payment") {
+                $link = route('gateway.showPayment', $params);
+              } else {
+                $link = route('gateway.showOrder', $params);
+              }
+
+              return '<a href="'.$link.'" target="_blank">Ver detalle</a>';
             }),
             AdminColumn::datetime('created_at')
               ->setLabel('Date')
